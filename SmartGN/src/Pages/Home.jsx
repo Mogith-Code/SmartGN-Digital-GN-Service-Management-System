cb11bda5ba680c90640b149a733bc53249a71edd
Achi-git <22cse0361@ms.sab.ac.lk>
2026-06-05 12:55:48 +0530

    Modified Home.jsx file

--- SmartGN/src/Pages/Home.jsx
+++ SmartGN/src/Pages/Home.jsx
@@ -10,7 +10,7 @@ function Home() {
     >
       <div className="w-full flex flex-col justify-between items-center gap-5">
         
-        /* IMAGE CONTAINER */
+        {/* IMAGE CONTAINER */}
         <div className="w-full h-auto "> 
           <img 
             src="/hero-image.png"
@@ -25,3 +25,31 @@ function Home() {
             in just a few clicks.
           </p>
         </div>
+        <div className="w-full flex items-center justify-center gap-5 max-sm:gap-3"> 
+          
+          {/* LOGIN BUTTON */}
+          <button 
+            className="flex items-center justify-center gap-2.5 px-[50px] py-2.5 bg-[#1B365D] shadow-[0_3px_10px_rgba(0,0,0,0.5)] text-[#F7FAFC] font-medium text-[16px] rounded-[15px] hover:bg-[#005BBD] hover:text-white transition-all duration-300 cursor-pointer max-sm:px-8 max-sm:py-2"
+            onClick={() => console.log('Login clicked')}
+            aria-label="Login to your account"
+          >
+            <span>Login</span>
+            <img src={loginIcon} alt="Login Icon" className="w-5 h-5" />
+          </button>
+
+          {/* REGISTER BUTTON */}
+          <button 
+            className="flex items-center justify-center gap-2.5 px-[50px] py-2.5 bg-[#D69E2E] shadow-[0_3px_10px_rgba(0,0,0,0.5)] text-[#F7FAFC] font-medium text-[16px] rounded-[15px] hover:bg-[#FFAA00] hover:text-white transition-all duration-300 cursor-pointer max-sm:px-8 max-sm:py-2"
+            onClick={() => console.log('Register clicked')}
+            aria-label="Register for a new account"
+          >
+            <span>Register</span>
+           <img src={registerIcon} alt="Register Icon" className="w-5 h-5" />
+          </button>
+        </div>
+      </div>
+    </section>
+  )
+}
+
+export default Home
