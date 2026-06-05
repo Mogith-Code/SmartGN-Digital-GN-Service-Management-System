eb94b1c22d8cec2d464b4dfc93677e560cd3b9f9
Achi-git <22cse0361@ms.sab.ac.lk>
2026-06-05 12:29:20 +0530

    Added Home.jsx file

---
+++ SmartGN/src/Pages/Home.jsx
@@ -0,0 +1,3 @@
+import React from 'react'
+import loginIcon from '../assets/images/login_24dp_F7FAFC_FILL0_wght400_GRAD0_opsz24.svg';
+import registerIcon from '../assets/images/how_to_reg_24dp_F7FAFC_FILL0_wght400_GRAD0_opsz24.svg';
