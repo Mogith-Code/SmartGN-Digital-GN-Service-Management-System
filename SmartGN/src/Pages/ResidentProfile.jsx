f0979f948419d9f93b27a2efd0e188b859252ecc
Achi-git <22cse0361@ms.sab.ac.lk>
2026-06-09 08:09:41 +0530

    added ResidentProfile.jsx file

---
+++ SmartGN/src/Pages/ResidentProfile.jsx
@@ -0,0 +1,4 @@
+import { useState, useEffect } from 'react'
+import { useLocation, useNavigate } from 'react-router-dom'
+import { translations, useLanguage } from '../utils/translate'
+import LanguageSelector from '../components/LanguageSelector'
