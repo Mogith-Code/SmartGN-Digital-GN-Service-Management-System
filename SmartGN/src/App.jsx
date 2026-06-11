7c93b1e5b389ddd320ad5f60a95fc7793c0c5957
Mogith-Code <kmogith1@gmail.com>
2026-06-11 12:19:01 +0530

    Registartion page connected

--- SmartGN/src/App.jsx
+++ SmartGN/src/App.jsx
@@ -4,6 +4,7 @@ import LandingPage from './Pages/LandingPage.jsx'
 import RAppointment from './Pages/RAppointment.jsx';
 import Login from './Pages/Login.jsx';
 import RHousehold from './Pages/RHousehold.jsx';
+import Register from './Pages/Registration.jsx';
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
         <Route path="/" element={<LandingPage />} />
         <Route path="/RAppointment" element={<RAppointment />} />
         <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
         <Route path="/RHousehold" element={<RHousehold />} />
       </Routes>
     </Router>
