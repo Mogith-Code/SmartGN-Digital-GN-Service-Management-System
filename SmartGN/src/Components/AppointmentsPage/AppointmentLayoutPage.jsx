d0c24b024b6045cfd0ce9a82732f041f33adf078
Janith Chamodya Warapitiya <warapitiyalakshan@gmail.com>
2026-06-13 21:45:41 +0530

    Update AppointmentLayoutPage.jsx

--- SmartGN/src/Components/AppointmentsPage/AppointmentLayoutPage.jsx
+++ SmartGN/src/Components/AppointmentsPage/AppointmentLayoutPage.jsx
@@ -1,7 +1,63 @@
 import React from "react";
+import pendingIcon from "../../assets/schedule_24dp_D69E2E_FILL0_wght400_GRAD0_opsz24.svg";
+import approvedIcon from "../../assets/verified_24dp_D69E2E_FILL0_wght400_GRAD0_opsz24.svg";
+import addIcon from "../../assets/add_24dp_D69E2E_FILL0_wght400_GRAD0_opsz24.svg";
 
 function AppointmentLayoutPage() {
-  return <div>AppointmentLayoutPage</div>;
+  return (
+    <>
+      <div className="flex text-[24px] font-medium text-[#1B365D] mt-[60px] mx-[30px] border border-[red]">
+        Appointments
+      </div>
+
+      <div className="grid grid-cols-3 gap-6 mx-[75px] mt-[30px]">
+        <div className="bg-[#E2E8F0] gap-[15px] rounded-2xl border border-[#2D37482D] p-[15px] flex flex-col items-center">
+          <img
+            src={pendingIcon}
+            alt="pendingIcon"
+            className="w-[50px] border border-[red]"
+          />
+
+          <div className="flex flex-col gap-[10px] border border-[red] items-center">
+            <span className="text-[16px] font-regular text-[#2D3748]">
+              Pending appointment requests
+            </span>
+            <span className="text-[20px] font-medium text-[#2D3748]">5</span>
+          </div>
+        </div>
+
+        <div className="bg-[#E2E8F0] gap-[15px] rounded-2xl border border-[#2D37482D] p-[15px] flex flex-col items-center">
+          <img
+            src={approvedIcon}
+            alt="approvedIcon"
+            className="w-[50px] border border-[red]"
+          />
+
+          <div className="flex flex-col gap-[10px] border border-[red] items-center">
+            <span className="text-[16px] font-regular text-[#2D3748]">
+              Approved appointment requests
+            </span>
+            <span className="text-[20px] font-medium text-[#2D3748]">5</span>
+          </div>
+        </div>
+
+        <div className="bg-[#E2E8F0] gap-[15px] rounded-2xl p-[15px] flex flex-col items-center shadow-[0px_2px_10px_rgba(0,0,0,0.5)] hover:shadow-[0px_2px_15px_rgba(0,0,0,0.6)] cursor-pointer">
+          <img
+            src={addIcon}
+            alt="pendingIcon"
+            className="w-[50px] border border-[red]"
+          />
+
+          <div className="flex flex-col gap-[10px] border border-[red] items-center">
+            <span className="text-[16px] font-regular text-[#2D3748]">
+              Book an appointment
+            </span>
+            <span className="text-[20px] font-medium text-[#2D3748]">5</span>
+          </div>
+        </div>
+      </div>
+    </>
+  );
 }
 
 export default AppointmentLayoutPage;
--- SmartGN/src/Pages/RAppointment.jsx
+++ SmartGN/src/Pages/RAppointment.jsx
@@ -1,19 +1,20 @@
 import React from "react";
 import AfterlogNavbar from "../Components/Common/AfterlogNavbar";
 import RSidebar from "../Components/Common/RSidebar";
+import AppointmentLayoutPage from "../Components/AppointmentsPage/AppointmentLayoutPage";
 
 function RAppointment() {
   return (
     <div className="w-full min-h-screen bg-[#F7FAFC] text-[#2D3748] flex flex-col">
       <AfterlogNavbar />
       <div className="flex gap-[20px]">
-        <div className=" bg-[#FFFFFF] border border-[red]">
+        <div className="flex bg-[#FFFFFF]">
           {/* Sidebar content */}
           <RSidebar />
         </div>
 
-        <div className="w-full bg-[#FFFFFF] p-4 border-l border-[#2D37482D]">
-          <h2 className="text-lg font-medium text-[#1B365D] mb-4">Dashboard</h2>
+        <div className="w-full bg-[#FFFFFF] border-l border-[#2D37482D]">
+          <AppointmentLayoutPage />
         </div>
       </div>
     </div>
