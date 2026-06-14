9be047327d67e19856710d633e6129f1157ed6fc
Janith Chamodya Warapitiya <warapitiyalakshan@gmail.com>
2026-06-15 03:11:01 +0530

    update bookingForm.jsx

--- SmartGN/src/Components/AppointmentsPage/BookingForm.jsx
+++ SmartGN/src/Components/AppointmentsPage/BookingForm.jsx
@@ -4,9 +4,19 @@ import RSidebar from "../Common/RSidebar";
 import Footer from "../Common/Footer";
 import backIcon from "../../assets/arrow_back_24dp_2D3748_FILL0_wght400_GRAD0_opsz24.svg";
 import { useNavigate } from "react-router-dom";
+import { useState } from "react";
 
 function BookingForm() {
+  // Booking Form States
+  const [purpose, setPurpose] = useState("Certificate Collection");
+  const [bookDay, setBookDay] = useState(17);
+  const [bookTime, setBookTime] = useState("2:00 PM");
+  const [officerName, setOfficerName] = useState("Kamal Silva");
+  const [contactNumber, setContactNumber] = useState("");
+  const [errorMessage, setErrorMessage] = useState("");
+
   const navigate = useNavigate();
+
   return (
     <div className="w-full min-h-screen bg-[#F7FAFC] text-[#2D3748] flex flex-col">
       <AfterlogNavbar />
@@ -24,6 +34,38 @@ function BookingForm() {
             <img src={backIcon} alt="pendingIcon" className="w-[16px]" />
             Back
           </div>
+
+          <div className="flex text-[24px] font-medium text-[#1B365D] border-b-[1.5px] border-[#2D37482D] pb-3 mb-5 mt-[30px] mx-[30px]">
+            Book New Appointment
+          </div>
+
+          <form>
+            <div className="mx-[50px] gap-5 border border-[#2D37482D] rounded-[15px] p-[20px]">
+              <div className="flex flex-col items-start gap-[2px] text-[16px] font-regular text-[#2D3748]">
+                <label htmlFor="purposeSelect">Appointment Purpose:</label>
+                <div className="flex bg-[#E2E8F0] border border-[#2D37484D] rounded-[5px] p-[5px] cursor-pointer">
+                  <select
+                    id="purposeSelect"
+                    value={purpose}
+                    onChange={(e) => setPurpose(e.target.value)}
+                    required
+                  >
+                    <option value="Certificate Collection">
+                      Certificate Collection
+                    </option>
+                    <option value="General Inquiry">General Inquiry</option>
+                    <option value="Family Details Verification">
+                      Family Details Verification
+                    </option>
+                    <option value="Allowance Inquiry">Allowance Inquiry</option>
+                    <option value="Land Dispute Negotiation">
+                      Land Dispute Negotiation
+                    </option>
+                  </select>
+                </div>
+              </div>
+            </div>
+          </form>
         </div>
       </div>
 
