f1227fbd78334ab1c09fd58948ecb4eda93c93da
Achi-git <22cse0361@ms.sab.ac.lk>
2026-06-17 22:34:10 +0530

    RAllowances file added

---
+++ SmartGN/src/Pages/RAllowances.jsx
@@ -0,0 +1,11 @@
+import { useState, useEffect } from 'react'
+import { useLocation, useNavigate } from 'react-router-dom'
+import { translations, useLanguage } from '../utils/translate'
+import LanguageSelector from '../components/LanguageSelector'
+import { getAuthHeaders } from '../utils/api'
+
+function ResidentAllowances({ onOpenHelp }) {
+  const navigate = useNavigate()
+  const location = useLocation()
+  const { lang } = useLanguage()
+  const t = translations[lang]
