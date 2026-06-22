93c97d52a4b919aa8157e0354b85b3fa86d0b995
Janith Chamodya Warapitiya <warapitiyalakshan@gmail.com>
2026-06-23 00:05:00 +0530

    import FamilyCardLyout from ./FamilyCardLyout

--- SmartGN/src/Components/Family&HouseholdPage/FamilyHouseholdLayout.jsx
+++ SmartGN/src/Components/Family&HouseholdPage/FamilyHouseholdLayout.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { useLanguage } from "../../utils/translate";
+import FamilyCardLyout from "./FamilyCardLyout";
 
 function FamilyHouseholdLayout() {
   const { lang } = useLanguage();
@@ -16,9 +17,15 @@ function FamilyHouseholdLayout() {
     FamilyHouseholdLayoutTranslations.EN;
 
   return (
-    <div className="flex text-xl sm:text-2xl md:text-3xl lg:text-[24px] font-medium text-[#1B365D] border-b border-[#2D37482D] pb-2 sm:pb-2.5 md:pb-3 lg:pb-[10px] mt-12 sm:mt-14 md:mt-16 lg:mt-[60px] mx-4 sm:mx-6 md:mx-8 lg:mx-[30px]">
-      {t.Title}
-    </div>
+    <>
+      <div className="flex text-xl sm:text-2xl md:text-3xl lg:text-[24px] font-medium text-[#1B365D] border-b border-[#2D37482D] pb-2 sm:pb-2.5 md:pb-3 lg:pb-[10px] mt-12 sm:mt-14 md:mt-16 lg:mt-[60px] mx-4 sm:mx-6 md:mx-8 lg:mx-[30px]">
+        {t.Title}
+      </div>
+
+      <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4 sm:gap-5 md:gap-6 mx-4 sm:mx-6 md:mx-8 lg:mx-[75px] mt-4 sm:mt-5 md:mt-6 lg:mt-[30px]">
+        <FamilyCardLyout />
+      </div>
+    </>
   );
 }
 
