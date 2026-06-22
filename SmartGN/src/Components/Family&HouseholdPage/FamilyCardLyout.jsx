036d36a4cff09b76557557dd24cf68d73feb2a85
Janith Chamodya Warapitiya <warapitiyalakshan@gmail.com>
2026-06-23 00:23:56 +0530

    Create card2

--- SmartGN/src/Components/Family&HouseholdPage/FamilyCardLyout.jsx
+++ SmartGN/src/Components/Family&HouseholdPage/FamilyCardLyout.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import { useLanguage } from "../../utils/translate";
 import totalMembersIcon from "../../assets/groups_24dp_D69E2E_FILL0_wght400_GRAD0_opsz24.svg";
 import { useNavigate } from "react-router-dom";
+import adultIcon from "../../assets/18_up_rating_24dp_D69E2E_FILL0_wght400_GRAD0_opsz24.svg";
 
 function FamilyCardLyout() {
   const { lang } = useLanguage();
@@ -19,7 +20,7 @@ function FamilyCardLyout() {
 
     SI: {
       Card1Title: "මුළු සාමාජිකයින්",
-      Card2Title: "වැඩිහිටි සාමාජිකයින්(18+)",
+      Card2Title: "වැඩිහිටි සාමාජිකයින් (18+)",
       Card3Title: "ළමුන්",
     },
 
@@ -34,19 +35,39 @@ function FamilyCardLyout() {
   const t =
     FamilyCardLayoutTranslations[lang] || FamilyCardLayoutTranslations.EN;
   return (
-    <div
-      className="bg-[#E2E8F0] gap-[15px] rounded-2xl p-[15px] flex flex-col items-center shadow-[0px_5px_10px_rgba(0,0,0,0.2)] hover:shadow-[0px_5px_15px_rgba(0,0,0,0.2)] hover:scale-102 transition-all duration-100 cursor-pointer"
-      onClick={() => navigate("/")}
-    >
-      <img src={totalMembersIcon} alt="totalMembersIcon" className="w-[50px]" />
+    <>
+      <div
+        className="bg-[#E2E8F0] gap-[15px] rounded-2xl p-[15px] flex flex-col items-center shadow-[0px_5px_10px_rgba(0,0,0,0.2)] hover:shadow-[0px_5px_15px_rgba(0,0,0,0.2)] hover:scale-102 transition-all duration-100 cursor-pointer"
+        onClick={() => navigate("/")}
+      >
+        <img
+          src={totalMembersIcon}
+          alt="totalMembersIcon"
+          className="w-[50px]"
+        />
 
-      <div className="flex flex-col gap-[10px] items-center">
-        <span className="text-[16px] font-regular text-[#2D3748] text-center">
-          {t.Card1Title}
-        </span>
-        <span className="text-[20px] font-medium text-[#2D3748]">5</span>
+        <div className="flex flex-col gap-[10px] items-center">
+          <span className="text-[16px] font-regular text-[#2D3748] text-center">
+            {t.Card1Title}
+          </span>
+          <span className="text-[20px] font-medium text-[#2D3748]">5</span>
+        </div>
       </div>
-    </div>
+
+      <div
+        className="bg-[#E2E8F0] gap-[15px] rounded-2xl p-[15px] flex flex-col items-center shadow-[0px_5px_10px_rgba(0,0,0,0.2)] hover:shadow-[0px_5px_15px_rgba(0,0,0,0.2)] hover:scale-102 transition-all duration-100 cursor-pointer"
+        onClick={() => navigate("/")}
+      >
+        <img src={adultIcon} alt="adultIcon" className="w-[50px]" />
+
+        <div className="flex flex-col gap-[10px] items-center">
+          <span className="text-[16px] font-regular text-[#2D3748] text-center">
+            {t.Card2Title}
+          </span>
+          <span className="text-[20px] font-medium text-[#2D3748]">2</span>
+        </div>
+      </div>
+    </>
   );
 }
 
