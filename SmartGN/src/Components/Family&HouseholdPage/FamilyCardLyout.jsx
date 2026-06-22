ef5e8da277375314690e5b648da8b51062c321dd
Janith Chamodya Warapitiya <warapitiyalakshan@gmail.com>
2026-06-23 00:10:56 +0530

    Add language translations

--- SmartGN/src/Components/Family&HouseholdPage/FamilyCardLyout.jsx
+++ SmartGN/src/Components/Family&HouseholdPage/FamilyCardLyout.jsx
@@ -1,6 +1,31 @@
 import React from "react";
+import { useLanguage } from "../../utils/translate";
 
 function FamilyCardLyout() {
+  const { lang } = useLanguage();
+
+  // TRANSLATION OBJECTS
+  // Contains all text content in three languages: English (EN),
+  // Sinhala (SI), and Tamil (TA)
+  const FamilyCardLayoutTranslations = {
+    EN: {
+      Card1Title: "Total members",
+      Card2Title: "Adult members (18+)",
+      Card3Title: "Children",
+    },
+
+    SI: {
+      Card1Title: "මුළු සාමාජිකයින්",
+      Card2Title: "වැඩිහිටි සාමාජිකයින්(18+)",
+      Card3Title: "ළමුන්",
+    },
+
+    TA: {
+      Card1Title: "மொத்த உறுப்பினர்கள்",
+      Card2Title: "வயது 18+ பெரியவர்கள்",
+      Card3Title: "குழந்தைகள்",
+    },
+  };
   return <div>FamilyCardLyout</div>;
 }
 
