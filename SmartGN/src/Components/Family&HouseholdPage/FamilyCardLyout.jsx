fe83eecd9562c084710b76d22eced7ad22cfc693
Janith Chamodya Warapitiya <warapitiyalakshan@gmail.com>
2026-06-23 00:29:01 +0530

    Create card3

--- SmartGN/src/Components/Family&HouseholdPage/FamilyCardLyout.jsx
+++ SmartGN/src/Components/Family&HouseholdPage/FamilyCardLyout.jsx
@@ -3,6 +3,7 @@ import { useLanguage } from "../../utils/translate";
 import totalMembersIcon from "../../assets/groups_24dp_D69E2E_FILL0_wght400_GRAD0_opsz24.svg";
 import { useNavigate } from "react-router-dom";
 import adultIcon from "../../assets/18_up_rating_24dp_D69E2E_FILL0_wght400_GRAD0_opsz24.svg";
+import childrenIcon from "../../assets/child_care_24dp_D69E2E_FILL0_wght400_GRAD0_opsz24.svg";
 
 function FamilyCardLyout() {
   const { lang } = useLanguage();
@@ -65,6 +66,20 @@ function FamilyCardLyout() {
             {t.Card2Title}
           </span>
           <span className="text-[20px] font-medium text-[#2D3748]">2</span>
+        </div>
+      </div>
+
+      <div
+        className="bg-[#E2E8F0] gap-[15px] rounded-2xl p-[15px] flex flex-col items-center shadow-[0px_5px_10px_rgba(0,0,0,0.2)] hover:shadow-[0px_5px_15px_rgba(0,0,0,0.2)] hover:scale-102 transition-all duration-100 cursor-pointer"
+        onClick={() => navigate("/")}
+      >
+        <img src={childrenIcon} alt="childrenIcon" className="w-[50px]" />
+
+        <div className="flex flex-col gap-[10px] items-center">
+          <span className="text-[16px] font-regular text-[#2D3748] text-center">
+            {t.Card3Title}
+          </span>
+          <span className="text-[20px] font-medium text-[#2D3748]">3</span>
         </div>
       </div>
     </>
