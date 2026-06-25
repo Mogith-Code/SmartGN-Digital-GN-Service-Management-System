a75a35ed075db49702f4c08a75495d0b70d93f6d
Achi-git <22cse0361@ms.sab.ac.lk>
2026-06-25 21:25:08 +0530

    Updated OfficerDisasterReport file

--- SmartGN/src/Pages/OfficerDisasterReport.jsx
+++ SmartGN/src/Pages/OfficerDisasterReport.jsx
@@ -10,3 +10,9 @@ function OfficerDisasterReports({ onOpenHelp }) {
   const { lang } = useLanguage()
   const t = translations[lang]
 
+   // Retrieve username and officerId from navigation state or localStorage
+  const successUser = location.state?.successUser || localStorage.getItem('smartgn_user_name') || 'Kamal Perera'
+  const officerIdVal = location.state?.officerId || localStorage.getItem('smartgn_user_id') || 'GN-BORELLA'
+  const firstName = successUser.split(' ')[0]
+
+
