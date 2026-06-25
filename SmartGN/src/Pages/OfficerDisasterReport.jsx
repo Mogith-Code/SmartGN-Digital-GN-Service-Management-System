eab03f6f5700c5915cf8b4b2ba986a60e8bd1a5c
Achi-git <22cse0361@ms.sab.ac.lk>
2026-06-25 21:23:51 +0530

    Updated OfficerDisasterReport file

--- SmartGN/src/Pages/OfficerDisasterReport.jsx
+++ SmartGN/src/Pages/OfficerDisasterReport.jsx
@@ -3,3 +3,10 @@ import { useLocation, useNavigate } from 'react-router-dom'
 import { translations, useLanguage } from '../utils/translate'
 import LanguageSelector from '../components/LanguageSelector'
 import { getAuthHeaders } from '../utils/api'
+
+function OfficerDisasterReports({ onOpenHelp }) {
+  const navigate = useNavigate()
+  const location = useLocation()
+  const { lang } = useLanguage()
+  const t = translations[lang]
+
