2d1f1b53a4fe5a6d2cf9810d9f7033964831e6d5
Janith Chamodya Warapitiya <warapitiyalakshan@gmail.com>
2026-06-27 06:42:31 +0530

    update EditFamilyDetails.jsx

--- SmartGN/src/Components/Family&HouseholdPage/EditFamilyDetails.jsx
+++ SmartGN/src/Components/Family&HouseholdPage/EditFamilyDetails.jsx
@@ -1,7 +1,11 @@
 import React from "react";
 
 function EditFamilyDetails() {
-  return <div>EditFamilyDetails</div>;
+  return (
+    <div className="w-full min-h-screen bg-[#F7FAFC] text-[#2D3748] flex flex-col">
+      EditFamilyDetails
+    </div>
+  );
 }
 
 export default EditFamilyDetails;
