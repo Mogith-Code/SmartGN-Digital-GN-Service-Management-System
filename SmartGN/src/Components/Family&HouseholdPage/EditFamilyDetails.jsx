2d476f39525520eda92ec9803c846a91bf7c8aea
Janith Chamodya Warapitiya <warapitiyalakshan@gmail.com>
2026-06-27 06:45:17 +0530

    add RSidebar.jsx

--- SmartGN/src/Components/Family&HouseholdPage/EditFamilyDetails.jsx
+++ SmartGN/src/Components/Family&HouseholdPage/EditFamilyDetails.jsx
@@ -1,10 +1,17 @@
 import React from "react";
 import AfterlogNavbar from "../Common/AfterlogNavbar";
+import RSidebar from "../Common/RSidebar";
 
 function EditFamilyDetails() {
   return (
     <div className="w-full min-h-screen bg-[#F7FAFC] text-[#2D3748] flex flex-col">
       <AfterlogNavbar />
+      <div className="flex flex-1 flex-col md:flex-row gap-0 md:gap-[20px]">
+        {/* Sidebar - Hidden on mobile, visible on md and up */}
+        <div className="hidden md:block bg-white">
+          <RSidebar />
+        </div>
+      </div>
     </div>
   );
 }
