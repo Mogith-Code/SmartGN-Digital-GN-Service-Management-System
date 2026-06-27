503fc1ad4601db13765e29ad3edc97421ca63304
Janith Chamodya Warapitiya <warapitiyalakshan@gmail.com>
2026-06-27 06:43:47 +0530

    add AfterlogNavbar.jsx

--- SmartGN/src/Components/Family&HouseholdPage/EditFamilyDetails.jsx
+++ SmartGN/src/Components/Family&HouseholdPage/EditFamilyDetails.jsx
@@ -1,9 +1,10 @@
 import React from "react";
+import AfterlogNavbar from "../Common/AfterlogNavbar";
 
 function EditFamilyDetails() {
   return (
     <div className="w-full min-h-screen bg-[#F7FAFC] text-[#2D3748] flex flex-col">
-      EditFamilyDetails
+      <AfterlogNavbar />
     </div>
   );
 }
