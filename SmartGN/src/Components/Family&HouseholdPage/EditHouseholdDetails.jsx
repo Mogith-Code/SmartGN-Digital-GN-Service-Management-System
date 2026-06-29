d27ee4b8f222aad7fad4d3b0929ca79bc7563672
Janith Chamodya Warapitiya <warapitiyalakshan@gmail.com>
2026-06-30 05:03:03 +0530

    update EditHouseholdDetails.jsx

--- SmartGN/src/Components/Family&HouseholdPage/EditHouseholdDetails.jsx
+++ SmartGN/src/Components/Family&HouseholdPage/EditHouseholdDetails.jsx
@@ -1,7 +1,12 @@
 import React from "react";
+import AfterlogNavbar from "../Common/AfterlogNavbar";
 
 function EditHouseholdDetails() {
-  return <div>EditHouseholdDetails</div>;
+  return (
+    <div className="w-full min-h-screen bg-[#F7FAFC] text-[#2D3748] flex flex-col">
+      <AfterlogNavbar />
+    </div>
+  );
 }
 
 export default EditHouseholdDetails;
