9c16cb388cf4af29a7e79b70e654292af2d981a9
Janith Chamodya Warapitiya <warapitiyalakshan@gmail.com>
2026-06-30 05:05:32 +0530

    Create  EditHouseholdDetailsLayout.jsx

---
+++ SmartGN/src/Components/Family&HouseholdPage/EditHouseHoldDetailsLayout.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function EditHouseHoldDetailsLayout() {
+  return <div>EditHouseHoldDetailsLayout</div>;
+}
+
+export default EditHouseHoldDetailsLayout;
--- SmartGN/src/Components/Family&HouseholdPage/EditHouseholdDetails.jsx
+++ SmartGN/src/Components/Family&HouseholdPage/EditHouseholdDetails.jsx
@@ -1,10 +1,19 @@
 import React from "react";
 import AfterlogNavbar from "../Common/AfterlogNavbar";
+import RSidebar from "../Common/RSidebar";
 
 function EditHouseholdDetails() {
   return (
     <div className="w-full min-h-screen bg-[#F7FAFC] text-[#2D3748] flex flex-col">
       <AfterlogNavbar />
+      <div className="flex flex-1 flex-col md:flex-row gap-0 md:gap-[20px]">
+        {/* Sidebar - Hidden on mobile, visible on md and up */}
+        <div className="hidden md:block bg-white">
+          <RSidebar />
+        </div>
+
+        <div className="w-full bg-white border-l-0 md:border-l border-[#2D37482D]"></div>
+      </div>
     </div>
   );
 }
