91c827b6d2298b4b97dc2f365c83045355fc5884
Janith Chamodya Warapitiya <warapitiyalakshan@gmail.com>
2026-07-03 03:16:21 +0530

    Update OfficerAppointmentLayout.jsx

--- SmartGN/src/Components/AppointmentsPage/OfficerAppointmentsLayoutPage.jsx
+++ SmartGN/src/Components/AppointmentsPage/OfficerAppointmentsLayoutPage.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { useLanguage } from "../../utils/translate";
+import searchIcon from "../../assets/search_24dp_2D3748_FILL0_wght400_GRAD0_opsz24.svg";
 
 function OfficerAppointmentsLayoutPage() {
   const { lang } = useLanguage();
@@ -15,8 +16,18 @@ function OfficerAppointmentsLayoutPage() {
     AppointmentLayoutTranslations[lang] || AppointmentLayoutTranslations.EN;
   return (
     <>
-      <div className="flex text-xl sm:text-2xl md:text-3xl lg:text-[24px] font-medium text-[#1B365D] border-b border-[#2D37482D] pb-2 sm:pb-2.5 md:pb-3 lg:pb-[10px] mt-12 sm:mt-14 md:mt-16 lg:mt-[60px] mx-4 sm:mx-6 md:mx-8 lg:mx-[30px]">
-        {t.Title}
+      <div className="flex justify-between text-xl sm:text-2xl md:text-3xl lg:text-[24px] font-medium text-[#1B365D] border-b border-[#2D37482D] pb-2 sm:pb-2.5 md:pb-3 lg:pb-[10px] mt-12 sm:mt-14 md:mt-16 lg:mt-[60px] mx-4 sm:mx-6 md:mx-8 lg:mx-[30px]">
+        <span>{t.Title}</span>
+        <div className="flex bg-[#E2E8F0] border border-[#2D37482D] rounded-[15px] py-[10px] px-[30px] items-center gap-[10px]">
+          <img
+            src={searchIcon}
+            alt="Search Icon"
+            className="w-[15px] h-[15px] opacity-[50%]"
+          />
+          <span className="text-[16px] font-light text-[#2D3748] opacity-[50%]">
+            Search residents
+          </span>
+        </div>
       </div>
     </>
   );
