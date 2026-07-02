a038f510076d460f6526df2266ee96630083174b
Janith Chamodya Warapitiya <warapitiyalakshan@gmail.com>
2026-07-03 03:26:46 +0530

    Create OfficerCardLayout.jsx

--- SmartGN/src/Components/AppointmentsPage/OfficerAppointmentsLayoutPage.jsx
+++ SmartGN/src/Components/AppointmentsPage/OfficerAppointmentsLayoutPage.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { useLanguage } from "../../utils/translate";
 import searchIcon from "../../assets/search_24dp_2D3748_FILL0_wght400_GRAD0_opsz24.svg";
+import OfficerCardLayout from "./OfficerCardLayout";
 
 function OfficerAppointmentsLayoutPage() {
   const { lang } = useLanguage();
@@ -18,7 +19,7 @@ function OfficerAppointmentsLayoutPage() {
     <>
       <div className="flex justify-between text-xl sm:text-2xl md:text-3xl lg:text-[24px] font-medium text-[#1B365D] border-b border-[#2D37482D] pb-2 sm:pb-2.5 md:pb-3 lg:pb-[10px] mt-12 sm:mt-14 md:mt-16 lg:mt-[60px] mx-4 sm:mx-6 md:mx-8 lg:mx-[30px]">
         <span>{t.Title}</span>
-        <div className="flex bg-[#E2E8F0] border border-[#2D37482D] rounded-[5px] py-[10px] px-[30px] items-center gap-[10px]">
+        <div className="flex bg-[#E2E8F0] border border-[#2D37482D] rounded-[10px] py-[10px] px-[30px] items-center gap-[10px]">
           <img
             src={searchIcon}
             alt="Search Icon"
@@ -28,6 +29,10 @@ function OfficerAppointmentsLayoutPage() {
             Search residents
           </span>
         </div>
+      </div>
+
+      <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4 sm:gap-5 md:gap-6 mx-4 sm:mx-6 md:mx-8 lg:mx-[75px] mt-4 sm:mt-5 md:mt-6 lg:mt-[30px]">
+        <OfficerCardLayout />
       </div>
     </>
   );
