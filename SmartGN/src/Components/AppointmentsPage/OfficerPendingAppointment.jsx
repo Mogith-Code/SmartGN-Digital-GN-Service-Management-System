0e7f3a3d0f7dff327ce9ede7f2df2680f6b0953b
Janith Chamodya Warapitiya <warapitiyalakshan@gmail.com>
2026-07-05 04:46:51 +0530

    Updated OfficerPendingAppointment.jsx

--- SmartGN/src/Components/AppointmentsPage/OfficerPendingAppointment.jsx
+++ SmartGN/src/Components/AppointmentsPage/OfficerPendingAppointment.jsx
@@ -1,10 +1,23 @@
 import React from "react";
 import OfficerNavbar from "../Common/OfficerNavbar";
+import OSidebar from "../Common/OSidebar";
 
 function OfficerPendingAppointment() {
   return (
     <div className="w-full min-h-screen bg-[#F7FAFC] text-[#2D3748] flex flex-col">
       <OfficerNavbar />
+
+      {/* Main Content Area */}
+      <div className="flex flex-1 flex-col md:flex-row gap-0 md:gap-[20px]">
+        <div className="flex flex-1 w-full">
+          <OSidebar />
+        </div>
+
+        {/* Main Content */}
+        <div className="w-full bg-white border-l-0 md:border-l border-[#2D37482D]">
+          \
+        </div>
+      </div>
     </div>
   );
 }
