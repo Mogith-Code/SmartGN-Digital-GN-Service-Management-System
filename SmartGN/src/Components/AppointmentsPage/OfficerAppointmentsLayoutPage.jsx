192abdd7d93344f03715e81e5b160c290b3dbf84
Janith Chamodya Warapitiya <warapitiyalakshan@gmail.com>
2026-07-05 07:12:32 +0530

    Updated OfficerCardLayout.jsx

--- SmartGN/src/Components/AppointmentsPage/OfficerAppointmentsLayoutPage.jsx
+++ SmartGN/src/Components/AppointmentsPage/OfficerAppointmentsLayoutPage.jsx
@@ -119,6 +119,14 @@ function OfficerAppointmentsLayoutPage() {
       year: appointment.date.getFullYear(),
     }));
   };
+
+  // Calculate dynamic stats
+  const pendingCount = appointments.filter(
+    (item) => item.status === "Pending",
+  ).length;
+  const approvedCount = appointments.filter(
+    (item) => item.status === "Approved",
+  ).length;
   return (
     <>
       <div className="flex  text-xl sm:text-2xl md:text-3xl lg:text-[24px] font-medium text-[#1B365D] border-b border-[#2D37482D] pb-2 sm:pb-2.5 md:pb-3 lg:pb-[10px] mt-12 sm:mt-14 md:mt-16 lg:mt-[60px] mx-4 sm:mx-6 md:mx-8 lg:mx-[30px]">
@@ -126,7 +134,10 @@ function OfficerAppointmentsLayoutPage() {
       </div>
 
       <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4 sm:gap-5 md:gap-6 mx-4 sm:mx-6 md:mx-8 lg:mx-[75px] mt-4 sm:mt-5 md:mt-6 lg:mt-[30px]">
-        <OfficerCardLayout />
+        <OfficerCardLayout
+          pendingCount={pendingCount}
+          approvedCount={approvedCount}
+        />
       </div>
 
       <div className="flex mt-4 sm:mt-5 md:mt-6 lg:mt-[30px] mx-4 sm:mx-6 md:mx-8 lg:mx-[100px]">
