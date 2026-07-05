c3e3ca3d5d0afdc6ecd5d43f2a33c6f347f305b3
Janith Chamodya Warapitiya <warapitiyalakshan@gmail.com>
2026-07-05 06:20:37 +0530

    Updated OfficerApprovedAppointment.jsx

--- SmartGN/src/Components/AppointmentsPage/OfficerApprovedAppointment.jsx
+++ SmartGN/src/Components/AppointmentsPage/OfficerApprovedAppointment.jsx
@@ -1,10 +1,17 @@
 import React from "react";
 import AfterlogNavbar from "../Common/AfterlogNavbar";
+import OSidebar from "../Common/OSidebar";
 
 function OfficerApprovedAppointment() {
   return (
     <div className="w-full min-h-screen bg-[#F7FAFC] text-[#2D3748] flex flex-col">
       <AfterlogNavbar />
+
+      <div className="flex gap-[20px] flex-1">
+        <div className="flex bg-[#FFFFFF]">
+          <OSidebar />
+        </div>
+      </div>
     </div>
   );
 }
