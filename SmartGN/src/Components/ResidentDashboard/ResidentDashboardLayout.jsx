36fa1a7ea4468dcc1868457d5115f8d6a25516dc
Janith Chamodya Warapitiya <warapitiyalakshan@gmail.com>
2026-07-06 15:48:15 +0530

    Created ResidentCardLayout.jsx

--- SmartGN/src/Components/ResidentDashboard/ResidentDashboardLayout.jsx
+++ SmartGN/src/Components/ResidentDashboard/ResidentDashboardLayout.jsx
@@ -1,7 +1,32 @@
 import React from "react";
+import { useNavigate } from "react-router-dom";
+import { useLanguage } from "../../utils/translate";
+import ResidentCardLayout from "./ResidentCardLayout";
 
 function ResidentDashboardLayout() {
-  return <div>ResidentDashboardLayout</div>;
+  const navigate = useNavigate();
+  const { lang } = useLanguage();
+
+  // TRANSLATION OBJECTS
+  const AppointmentLayoutTranslations = {
+    EN: { Title: "Have a Nice Day Nimal!" },
+    SI: { Title: "හමුවවීම්" },
+    TA: { Title: "சந்திப்புகள்" },
+  };
+
+  const t =
+    AppointmentLayoutTranslations[lang] || AppointmentLayoutTranslations.EN;
+  return (
+    <>
+      <div className="flex text-xl sm:text-2xl md:text-3xl lg:text-[24px] font-medium text-[#1B365D] border-b border-[#2D37482D] pb-2 sm:pb-2.5 md:pb-3 lg:pb-[10px] mt-12 sm:mt-14 md:mt-16 lg:mt-[60px] mx-4 sm:mx-6 md:mx-8 lg:mx-[30px]">
+        {t.Title}
+      </div>
+
+      <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4 sm:gap-5 md:gap-6 mx-4 sm:mx-6 md:mx-8 lg:mx-[75px] mt-4 sm:mt-5 md:mt-6 lg:mt-[30px]">
+        <ResidentCardLayout />
+      </div>
+    </>
+  );
 }
 
 export default ResidentDashboardLayout;
