16bd0db0d0534aa28a9ecd508a7a682f8c00451a
Janith Chamodya Warapitiya <warapitiyalakshan@gmail.com>
2026-07-06 15:57:37 +0530

    Updated  ResidentCardLayout.jsx

--- SmartGN/src/Components/ResidentsDetails/ResidentsDetailsLayout.jsx
+++ SmartGN/src/Components/ResidentsDetails/ResidentsDetailsLayout.jsx
@@ -1,7 +1,6 @@
 import React from "react";
 import { useLanguage } from "../../utils/translate";
 import searchIcon from "../../assets/search_24dp_2D3748_FILL0_wght400_GRAD0_opsz24.svg";
-import ResidentsDetailsCardLAyout from "./ResidentsDetailsCardLAyout";
 
 function ResidentsDetailsLayout() {
   const { lang } = useLanguage();
@@ -30,10 +29,6 @@ function ResidentsDetailsLayout() {
             Search residents using NIC number
           </span>
         </div>
-      </div>
-
-      <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4 sm:gap-5 md:gap-6 mx-4 sm:mx-6 md:mx-8 lg:mx-[75px] mt-4 sm:mt-5 md:mt-6 lg:mt-[30px]">
-        <ResidentsDetailsCardLAyout />
       </div>
     </>
   );
