05e6e5ea222ae3c3d6a00f1991af5d4e12dbcd7b
Janith Chamodya Warapitiya <warapitiyalakshan@gmail.com>
2026-07-06 16:50:03 +0530

    Updated  ResidentDashboardLayout.jsx

--- SmartGN/src/Components/ResidentDashboard/ResidentDashboardLayout.jsx
+++ SmartGN/src/Components/ResidentDashboard/ResidentDashboardLayout.jsx
@@ -228,8 +228,24 @@ function ResidentDashboardLayout() {
         />
       </div>
 
-      <div className="flex mx-[100px] border border-[#2D37482D] rounded-[15px] bg-[#FDF5E6] mt-[30px] p-[20px]">
-        hii
+      <div className="flex mx-[100px] justify-center border border-[#2D37482D] rounded-[15px] bg-[#FDF5E6] mt-[30px] p-[20px]">
+        <div className="flex flex-col gap-[25px] justify-center">
+          <span className="text-[#1B365D] text-[20px]">Quick Actions</span>
+          <div className="grid grid-cols-2 gap-[20px]">
+            <button className="bg-[#FFFFFF] flex items-center justify-center p-[20px] rounded-[15px] shadow-[0px_5px_10px_rgba(0,0,0,0.2)] hover:shadow-[0px_5px_15px_rgba(0,0,0,0.2)] hover:scale-102 transition-all duration-100 cursor-pointer">
+              {" "}
+            </button>
+            <button className="bg-[#FFFFFF] flex items-center justify-center p-[20px] rounded-[15px] shadow-[0px_5px_10px_rgba(0,0,0,0.2)] hover:shadow-[0px_5px_15px_rgba(0,0,0,0.2)] hover:scale-102 transition-all duration-100 cursor-pointer">
+              {" "}
+            </button>
+            <button className="bg-[#FFFFFF] flex items-center justify-center p-[20px] rounded-[15px] shadow-[0px_5px_10px_rgba(0,0,0,0.2)] hover:shadow-[0px_5px_15px_rgba(0,0,0,0.2)] hover:scale-102 transition-all duration-100 cursor-pointer">
+              {" "}
+            </button>
+            <button className="bg-[#FFFFFF] flex items-center justify-center p-[20px] rounded-[15px] shadow-[0px_5px_10px_rgba(0,0,0,0.2)] hover:shadow-[0px_5px_15px_rgba(0,0,0,0.2)] hover:scale-102 transition-all duration-100 cursor-pointer">
+              {" "}
+            </button>
+          </div>
+        </div>
       </div>
     </>
   );
