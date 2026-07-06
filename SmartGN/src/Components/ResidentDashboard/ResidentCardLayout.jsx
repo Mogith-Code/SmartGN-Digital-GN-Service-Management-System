4215591ac450442db28ca13644d4814c623cbb5a
Janith Chamodya Warapitiya <warapitiyalakshan@gmail.com>
2026-07-06 15:58:40 +0530

    Updated  ResidentCardLayout.jsx

--- SmartGN/src/Components/ResidentDashboard/ResidentCardLayout.jsx
+++ SmartGN/src/Components/ResidentDashboard/ResidentCardLayout.jsx
@@ -13,9 +13,9 @@ function ResidentCardLayout({ pendingCount, approvedCount }) {
   // TRANSLATION OBJECTS
   const CardLayoutTranslations = {
     EN: {
-      Card1Title: "Pending appointment requests",
-      Card2Title: "Approved appointment requests",
-      Card3Title: "Book an appointment",
+      Card1Title: "Pending requests",
+      Card2Title: "Approved requests",
+      Card3Title: "Upcoming appointments",
     },
     SI: {
       Card1Title: "අනුමැතිය ලැබීමට නියමිත හමුවීම් සඳහා ඉල්ලීම්",
