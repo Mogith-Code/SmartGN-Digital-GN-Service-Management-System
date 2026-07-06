75f2f625d50f323b3f75165687345295276c019c
Janith Chamodya Warapitiya <warapitiyalakshan@gmail.com>
2026-07-06 16:02:31 +0530

    Updated  ResidentCardLayout.jsx

--- SmartGN/src/Components/ResidentDashboard/ResidentCardLayout.jsx
+++ SmartGN/src/Components/ResidentDashboard/ResidentCardLayout.jsx
@@ -1,9 +1,9 @@
 import React from "react";
 import { useState } from "react";
 import { useLanguage } from "../../utils/translate"; // Custom hook for multilingual support
-import pendingIcon from "../../assets/schedule_24dp_D69E2E_FILL0_wght400_GRAD0_opsz24.svg";
-import approvedIcon from "../../assets/verified_24dp_D69E2E_FILL0_wght400_GRAD0_opsz24.svg";
-import addIcon from "../../assets/add_24dp_D69E2E_FILL0_wght400_GRAD0_opsz24.svg";
+import totalPendingIcon from "../../assets/pending_actions_24dp_D69E2E_FILL0_wght400_GRAD0_opsz24.svg";
+import totalapprovedIcon from "../../assets/assignment_turned_in_24dp_D69E2E_FILL0_wght400_GRAD0_opsz24.svg";
+import upcomingIcon from "../../assets/event_upcoming_24dp_D69E2E_FILL0_wght400_GRAD0_opsz24.svg";
 import { useNavigate } from "react-router-dom";
 
 function ResidentCardLayout({ pendingCount, approvedCount }) {
@@ -51,21 +51,21 @@ function ResidentCardLayout({ pendingCount, approvedCount }) {
   const cards = [
     {
       id: 1,
-      icon: pendingIcon,
+      icon: totalPendingIcon,
       title: t.Card1Title,
       count: pendingCount,
       onClick: handleCard1Click,
     },
     {
       id: 2,
-      icon: approvedIcon,
+      icon: totalapprovedIcon,
       title: t.Card2Title,
       count: approvedCount,
       onClick: handleCard2Click,
     },
     {
       id: 3,
-      icon: addIcon,
+      icon: upcomingIcon,
       title: t.Card3Title,
       count: null,
       onClick: handleCard3Click,
