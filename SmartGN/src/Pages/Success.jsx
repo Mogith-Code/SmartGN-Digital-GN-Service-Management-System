b810c77f6b5da86a1e8424f3b78fb6b4680937b1
Mogith-Code <kmogith1@gmail.com>
2026-07-09 10:12:56 +0530

    success import added

---
+++ SmartGN/src/Pages/Success.jsx
@@ -0,0 +1,4 @@
+import { useNavigate, useLocation } from 'react-router-dom'
+import { useLanguage } from '../utils/translate'
+import LanguageSelector from '../Components/Common/LanguageSelector'
+import logoImage from '../assets/logo.png'
