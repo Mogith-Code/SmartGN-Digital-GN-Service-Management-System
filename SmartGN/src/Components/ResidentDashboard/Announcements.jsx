a69402d8972850b5387fd1524c1ce949de7ee2b8
Janith Chamodya Warapitiya <warapitiyalakshan@gmail.com>
2026-07-14 17:38:34 +0530

    Added Language translations

--- SmartGN/src/Components/ResidentDashboard/Announcements.jsx
+++ SmartGN/src/Components/ResidentDashboard/Announcements.jsx
@@ -1,7 +1,35 @@
 import React from "react";
 import announcementIcon from "../../assets/brand_awareness_24dp_D69E2E_FILL0_wght400_GRAD0_opsz24.svg";
+import { useLanguage } from "../../utils/translate";
 
 function Announcements() {
+  const { lang } = useLanguage();
+
+  const AnnTranslations = {
+    EN: {
+      title: "Announcements",
+      view: "View all",
+      Card2Title: "Book Appointments",
+      Card3Title: "Apply for Allowances",
+      Card4Title: "Disaster Relief",
+    },
+    SI: {
+      title: "ඇනවුන්ස්මන්ට්",
+      view: "සියල්ල දැක්වීම",
+      Card2Title: "හමුවීම් වෙන්කරන්න",
+      Card3Title: "සහන සඳහා අයදුම් කරන්න",
+      Card4Title: "අනතුරු සහන",
+    },
+    TA: {
+      title: "அறிவிப்புகள்",
+      view: "அனைத்தையும் பார்வையிடு",
+      Card2Title: "அங்கீகாரம் பெற்ற சந்திப்பு கோரிக்கைகள்",
+      Card3Title: "சந்திப்பை பதிவு செய்யவும்",
+      Card4Title: "விபத்து நிவாரணம்",
+    },
+  };
+
+  const t = AnnTranslations[lang] || AnnTranslations.EN;
   const Announcements = [
     {
       id: 1,
@@ -36,10 +64,10 @@ function Announcements() {
             <span>View all</span>
           </div>
           <div className="flex text-[#1B365D] font-medium text-[20px]">
-            Announcements
+            {t.title}
           </div>
           <div className="flex text-[#D69E2E] text-[16px] bover: cursor-pointer hover:underline hover:font-medium">
-            <span>View all</span>
+            <span>{t.view}</span>
           </div>
         </div>
 
