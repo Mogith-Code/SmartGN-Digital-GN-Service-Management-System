3fa7f96af46edac4325743d8d046f53852c565da
Achi-git <22cse0361@ms.sab.ac.lk>
2026-07-15 19:29:32 +0530

    Added ResidentDisasterReport file

--- SmartGN/src/Pages/ResidentDisasterReport.jsx
+++ SmartGN/src/Pages/ResidentDisasterReport.jsx
@@ -1,9 +1,9 @@
-import React from 'react';
-
-function ResidentDisasterReport() {
-  return (
-    <div></div>
-  );
-}
+import { useState, useEffect } from 'react'
+import { useLocation, useNavigate } from 'react-router-dom'
+import { translations, useLanguage } from '../utils/translate'
+import { getAuthHeaders } from '../utils/api'
+import AfterlogNavbar from '../Components/Common/AfterlogNavbar'
+import RSidebar from '../Components/Common/RSidebar'
+import Footer from '../Components/Common/Footer'
 
 export default ResidentDisasterReport;
