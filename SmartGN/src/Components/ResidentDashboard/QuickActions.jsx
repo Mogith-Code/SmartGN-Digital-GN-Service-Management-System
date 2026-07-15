5f0529d97f6c4b6098b320452fb7ea2f88bdc701
Janith Chamodya Warapitiya <warapitiyalakshan@gmail.com>
2026-07-15 19:53:00 +0530

    updated QuickActions.jsx

--- SmartGN/src/Components/ResidentDashboard/QuickActions.jsx
+++ SmartGN/src/Components/ResidentDashboard/QuickActions.jsx
@@ -1,6 +1,9 @@
 import React from "react";
 import certificateIcon from "../../assets/license_24dp_D69E2E_FILL0_wght400_GRAD0_opsz24.svg";
 import arrowIcon from "../../assets/arrow_forward_24dp_2D3748_FILL0_wght400_GRAD0_opsz24.svg";
+import allowancesIcon from "../../assets/edit_document_24dp_D69E2E_FILL0_wght400_GRAD0_opsz24.svg";
+import disasterIcon from "../../assets/flood_24dp_D69E2E_FILL0_wght400_GRAD0_opsz24.svg";
+import appointmentIcon from "../../assets/calendar_today_24dp_D69E2E_FILL0_wght400_GRAD0_opsz24.svg";
 import { useNavigate } from "react-router-dom";
 import { useLanguage } from "../../utils/translate";
 
@@ -45,14 +48,14 @@ function QuickActions() {
     {
       id: 2,
       title: t.Card2Title,
-      icon: certificateIcon,
+      icon: appointmentIcon,
       alt: "appointment icon",
       route: "/ResidentDashboard/RAppointment/BookingForm",
     },
     {
       id: 3,
       title: t.Card3Title,
-      icon: certificateIcon,
+      icon: allowancesIcon,
       alt: "allowance icon",
       route: "/ResidentDashboard/allowances",
     },
