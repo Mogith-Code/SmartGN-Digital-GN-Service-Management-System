cf67f5e4a793cd4988b1b029dfca28d6189ba1c2
Mogith-Code <kmogith1@gmail.com>
2026-07-16 16:54:01 +0530

    email otp component added

--- Backend/src/utils/emailService.js
+++ Backend/src/utils/emailService.js
@@ -1,4 +1,14 @@
 const fs = require('fs');
 const path = require('path');
 
+// We'll write to a local log file so developers/users can easily view sent emails
+const logFilePath = path.join(__dirname, '../../../sent_emails.log');
+
+/**
+ * Sends a 6-digit OTP code to the specified email address
+ * @param {string} email - Recipient email
+ * @param {string} otp - 6-digit OTP code
+ * @param {string} purpose - Purpose of OTP ('registration' or 'login')
+ * @returns {Promise<boolean>}
+ */
 
