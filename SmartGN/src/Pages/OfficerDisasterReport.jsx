098b874f384206125a3879817429085d214be06e
Achi-git <22cse0361@ms.sab.ac.lk>
2026-07-17 23:16:54 +0530

    Added OfficerDisasterReport

--- SmartGN/src/Pages/OfficerDisasterReport.jsx
+++ SmartGN/src/Pages/OfficerDisasterReport.jsx
@@ -1,9 +1,8 @@
-import React from 'react';
+import { useState, useEffect } from 'react'
+import { useLocation, useNavigate } from 'react-router-dom'
+import { translations, useLanguage } from '../utils/translate'
+import { getAuthHeaders } from '../utils/api'
+import OfficerNavbar from '../Components/Common/OfficerNavbar'
+import OSidebar from '../Components/Common/OSidebar'
+import Footer from '../Components/Common/Footer'
 
-function OfficerDisasterReports() {
-  return (
-    <div></div>
-  );
-}
-
-export default OfficerDisasterReports;
