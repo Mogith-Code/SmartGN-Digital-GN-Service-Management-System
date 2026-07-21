debb4b01c5b8b09dfc0cded98e0ee65cdbcf171a
Janith Chamodya Warapitiya <warapitiyalakshan@gmail.com>
2026-07-21 18:04:57 +0530

    added language translations for Rcertificates.jsx

--- SmartGN/src/Pages/RCertificates.jsx
+++ SmartGN/src/Pages/RCertificates.jsx
@@ -12,7 +12,26 @@ function ResidentCertificates({ onOpenHelp }) {
   const navigate = useNavigate();
   const location = useLocation();
   const { lang } = useLanguage();
-  const t = translations[lang];
+
+  const CertificateTranslations = {
+    EN: {
+      greeting: `Have a Nice Day, ${profile.firstName || "Resident"}!`,
+      alert:
+        "Please upload a high-quality image of your National Identity Card",
+    },
+    SI: {
+      greeting: `සුභ දවසක්, ${profile.firstName || "නේවාසික"}!`,
+      alert:
+        "කරුණාකර ඔබේ ජාතික හැඳුනුම්පත් පත්‍රයේ උසස් තත්ත්වයේ රූපයක් උඩුගත කරන්න",
+    },
+    TA: {
+      greeting: `இனிய நாள், ${profile.firstName || "குடியுரிமை"}!`,
+      alert:
+        "தயவுசெய்து உங்கள் தேசிய அடையாள அட்டையின் உயர் தரமான படத்தை பதிவேற்றவும்",
+    },
+  };
+
+  const t = CertificateTranslations[lang] || CertificateTranslations.EN;
 
   // State to manage dismissing the alert banner
   const [showAlert, setShowAlert] = useState(true);
@@ -159,9 +178,13 @@ function ResidentCertificates({ onOpenHelp }) {
               {showAlert && !profile.nicFront && !profile.nicBack && (
                 <div className="flex justify-between items-center p-[10px] bg-[#fef3c7] border border-[#fde68a] rounded-xl text-[#d97706] font-medium text-[14px] text-left z-1 ">
                   <div className="flex items-center gap-2">
-                    <span>
-                      Please upload a high-quality image of your National
-                      Identity Card
+                    <span
+                      className="hover:underline hover:cursor-pointer"
+                      onClick={() => {
+                        navigate("/ResidentDashboard/profile");
+                      }}
+                    >
+                      {t.alert}
                     </span>
                   </div>
                   <button
