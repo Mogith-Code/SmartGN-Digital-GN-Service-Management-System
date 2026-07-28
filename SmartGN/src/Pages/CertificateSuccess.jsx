15b3fae8413e69b217c72155ddffcae0f3ef1ba9
Mogith-Code <kmogith1@gmail.com>
2026-07-28 21:20:32 +0530

    certificate success page create

---
+++ SmartGN/src/Pages/CertificateSuccess.jsx
@@ -0,0 +1,6 @@
+import React from "react";
+import { useNavigate, useLocation } from "react-router-dom";
+import AfterlogNavbar from "../Components/Common/AfterlogNavbar";
+import RSidebar from "../Components/Common/RSidebar";
+import Footer from "../Components/Common/Footer";
+import { useLanguage } from "../utils/translate";
